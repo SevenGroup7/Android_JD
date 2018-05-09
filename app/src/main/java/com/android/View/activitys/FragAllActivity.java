458c2f2a1da507eee0ac58bad572294e599f5b82
Wittman0980 <17710928593@163.com>
2018-05-09 20:00:26 +0800

    主Activity

--- app/src/main/java/com/android/View/activitys/FragAllActivity.java
+++ app/src/main/java/com/android/View/activitys/FragAllActivity.java
@@ -3,13 +3,34 @@ package com.android.View.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.Presenter.MainPresenter;
 import com.android.R;
 
-public class FragAllActivity extends AppCompatActivity {
+public class FragAllActivity extends BaseActivity<MainPresenter> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frag_all);
+//        setContentView(R.layout.activity_frag_all);
+    }
+
+    @Override
+    void initView() {
+
+    }
+
+    @Override
+    void initDate() {
+
+    }
+
+    @Override
+    MainPresenter setPresenter() {
+        return new MainPresenter();
+    }
+
+    @Override
+    int setChildContentView() {
+        return R.layout.activity_frag_all;
     }
 }
