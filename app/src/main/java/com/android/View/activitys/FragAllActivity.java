61a9a62375750b52fe49f5e46592345ba639da45
Wittman0980 <17710928593@163.com>
2018-05-09 21:04:35 +0800

    底部导航栏

--- app/src/main/java/com/android/View/activitys/FragAllActivity.java
+++ app/src/main/java/com/android/View/activitys/FragAllActivity.java
@@ -1,12 +1,19 @@
 package com.android.View.activitys;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.android.Presenter.MainPresenter;
 import com.android.R;
+import com.android.View.fragments.ClassifyFragment;
+import com.android.View.fragments.HomeFragment;
+import com.android.View.fragments.MineFragment;
+import com.android.View.fragments.ShoppingCarFragment;
+import com.hjm.bottomtabbar.BottomTabBar;
 
 public class FragAllActivity extends BaseActivity<MainPresenter> {
+
+    private BottomTabBar tabBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +23,31 @@ public class FragAllActivity extends BaseActivity<MainPresenter> {
 
     @Override
     void initView() {
+        tabBar = findViewById(R.id.tabBar);
+
+        tabBar.init(getSupportFragmentManager())
+                .setImgSize(30,30)
+                .setFontSize(13)
+                .setChangeColor(Color.RED, Color.GRAY)
+                .setTabBarBackgroundColor(Color.LTGRAY)
+                .addTabItem("首页", R.drawable.home1, R.drawable.home0, HomeFragment.class)
+        //前面第一个是点击后的图片，第二个是点击前的图片
+                .addTabItem("分类", R.drawable.classify1, R.drawable.classify0, ClassifyFragment.class)
+                .addTabItem("购物车", R.drawable.shoppingcar1, R.drawable.shoppingcar0, ShoppingCarFragment.class)
+                .addTabItem("我的", R.drawable.mine1, R.drawable.mine0, MineFragment.class)
+                .isShowDivider(false);
 
     }
 
     @Override
     void initDate() {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.detachView();
     }
 
     @Override
