93166909773be44f81ed5aba934291cbe8fafd35
Wittman0980 <17710928593@163.com>
2018-05-16 14:54:43 +0800

    修改BottomBar

--- app/src/main/java/com/android/View/activitys/FragAllActivity.java
+++ app/src/main/java/com/android/View/activitys/FragAllActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.android.Presenter.MainPresenter;
@@ -17,7 +18,6 @@ import com.android.View.fragments.ShoppingCarFragment;
 import com.roughike.bottombar.BottomBar;
 
 import com.android.R;
-import com.roughike.bottombar.BottomBarTab;
 import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
 import com.roughike.bottombar.TabSelectionInterceptor;
@@ -35,54 +35,65 @@ public class FragAllActivity extends BaseActivity<MainPresenter> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_frag_all);
+
+
         initView();
         create();
     }
 
     @Override
     void initView() {
-        bottomBar = (BottomBar) findViewById(R.id.tabBar);
-        viewPager = findViewById(R.id.vp);
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        View layout = findViewById(R.id.contentContainer);
     }
 
     private void create() {
-        fragmentList = new ArrayList<>();
-        fragmentList.add(new HomeFragment());
-        fragmentList.add(new ClassifyFragment());
-        fragmentList.add(new ShoppingCarFragment());
-        fragmentList.add(new MineFragment());
-        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return fragmentList.get(position);
-            }
+        /**
+         * fragmentList = new ArrayList<>();
+         fragmentList.add(new HomeFragment());
+         fragmentList.add(new ClassifyFragment());
+         fragmentList.add(new ShoppingCarFragment());
+         fragmentList.add(new MineFragment());
+         viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        @Override public Fragment getItem(int position) {
+        return fragmentList.get(position);
+        }
 
-            @Override
-            public int getCount() {
-                return fragmentList.size();
-            }
+        @Override public int getCount() {
+        return fragmentList.size();
+        }
         });
+         */
 
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(int tabId) {
+                Object ob = null;
                 switch (tabId) {
                     case R.id.tab_home:
-                        viewPager.setCurrentItem(0);
+                        ob = new HomeFragment();
                         break;
 
                     case R.id.tab_classify:
-                        viewPager.setCurrentItem(1);
+                        ob = new ClassifyFragment();
                         break;
 
                     case R.id.tab_shoppingcar:
-                        viewPager.setCurrentItem(2);
+                        ob = new ShoppingCarFragment();
                         break;
 
                     case R.id.tab_mine:
-                        viewPager.setCurrentItem(3);
+                        ob = new MineFragment();
                         break;
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.contentContainer, (Fragment) ob).commit();
+            }
+        });
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(int tabId) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.contentContainer, new HomeFragment()).commit();
             }
         });
     }
